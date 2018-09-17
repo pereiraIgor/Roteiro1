ae94871f9d3504356b09ff2f7bb9baa399bc04a5
pereiraIgor <igor.pereira@ccc.ufcg.edu.br>
2018-09-17 19:46:33 -0300

    algoritimos implementados

--- R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/CombSort.java
+++ R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/CombSort.java
@@ -7,26 +7,30 @@ import util.Util;
  * The combsort algoritm.
  */
 public class CombSort<T extends Comparable<T>> extends AbstractSorting<T> {
-   @Override
-   public void sort(T[] array, int leftIndex, int rightIndex) {
-      double fator = 1.25;
-      int tamanhoTotal = rightIndex - leftIndex + 1;
-      int gap = (int) (tamanhoTotal / fator);
-
-      while (gap > 1) {
-         for (int i = leftIndex; i + gap <= rightIndex; i++) {
-            if (array[i].compareTo(array[i + gap]) > 0) {
-               Util.swap(array, i, i + gap);
-            }
-            gap /= fator;
-         }
-      }
-      for (int j = leftIndex; j <= rightIndex; j++) {
-         for (int i = j; i <= rightIndex; i++) {
-            if (array[j].compareTo(array[i]) > 0) {
-               Util.swap(array, j, i);
-            }
-         }
-      }
-   }
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		if (Util.verificaLimites(array, leftIndex, rightIndex)) {
+			double fator = 1.25;
+			int tamanhoTotal = rightIndex - leftIndex + 1;
+			int gap = (int) (tamanhoTotal / fator);
+			boolean ordenado = false;
+			while (gap >= 1 && !ordenado) {
+				int variavel = 0;
+				for (int i = leftIndex; i + gap <= rightIndex; i++) {
+					if (array[i].compareTo(array[i + gap]) > 0) {
+						Util.swap(array, i, i + gap);
+						variavel++;
+					}
+					if (gap == 1) {
+						gap = 1;
+					} else {
+						gap /= fator;
+					}
+				}
+				if (variavel == 0) {
+					ordenado = true;
+				}
+			}
+		}
+	}
 }
--- R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
+++ R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
@@ -7,19 +7,21 @@ import util.Util;
  * The implementation of the algorithm must be in-place!
  */
 public class GnomeSort<T extends Comparable<T>> extends AbstractSorting<T> {
-   public void sort(T[] array, int leftIndex, int rightIndex) {
-      int pivot = leftIndex + 1;
-      while (pivot < rightIndex) {
-         if (array[pivot].compareTo(array[pivot + 1]) > 0) {
-            Util.swap(array, pivot, pivot + 1);
-            if (pivot - 1 < leftIndex) {
-               pivot++;
-            } else {
-               pivot--;
-            }
-         } else {
-            pivot++;
-         }
-      }
-   }
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		if (Util.verificaLimites(array, leftIndex, rightIndex)) {
+			int pivot = leftIndex + 1;
+			while (pivot < rightIndex) {
+				if (array[pivot].compareTo(array[pivot + 1]) > 0) {
+					Util.swap(array, pivot, pivot + 1);
+					if (pivot - 1 < leftIndex) {
+						pivot++;
+					} else {
+						pivot--;
+					}
+				} else {
+					pivot++;
+				}
+			}
+		}
+	}
 }
