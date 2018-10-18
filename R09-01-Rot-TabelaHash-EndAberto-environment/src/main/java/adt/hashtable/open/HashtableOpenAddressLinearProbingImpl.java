27cf4c3d7e78dfb8a8032e5c9dd5b13942024b14
pereiraIgor <igor.pereira@ccc.ufcg.edu.br>
2018-10-18 15:51:28 -0300

    Resta analisar

--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -2,12 +2,11 @@ package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
+import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 
-public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
-		AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressLinearProbingImpl(int size,
-			HashFunctionClosedAddressMethod method) {
+	public HashtableOpenAddressLinearProbingImpl(int size, HashFunctionClosedAddressMethod method) {
 		super(size);
 		hashFunction = new HashFunctionLinearProbing<T>(size, method);
 		this.initiateInternalTable(size);
@@ -15,25 +14,59 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 
 	@Override
 	public void insert(T element) {
-		int index = ((HashtableOpenAddressLinearProbingImpl<Storable>) hashFunction).ha
+		int probe = 0;
+		int index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, probe);
+		while (table[index] != null && probe < table.length) {
+			index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, ++probe);
+			COLLISIONS++;
+		}
+		if (table[index] == null) {
+			table[index] = element;
+			elements++;
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int probe = 0;
+		int index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, probe);
+		while (table[index] != null && !table[index].equals(element) && probe < table.length) {
+			index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, ++probe);
+		}
+		if (table[index] != null && table[index].equals(element)) {
+			table[index] = deletedElement;
+			elements--;
+		}
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T retorno = null;
+		int probe = 0;
+		int index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, probe);
+		while (table[index] != null && !table[index].equals(element) && probe < table.length) {
+			index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, ++probe);
+		}
+
+		if (table[index] != null && table[index].equals(element)) {
+			retorno = (T) table[index];
+		}
+		return retorno;
 	}
 
 	@Override
 	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int indice = -1;
+		int probe = 0;
+		int index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, probe);
+		while (!table[index].equals(element) && probe < table.length && table[index] != null) {
+			index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, ++probe);
+		}
+		if (table[index].equals(element)) {
+			indice = index;
+		}
+		return indice;
+
 	}
 
 }
--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressQuadraticProbingImpl.java
@@ -1,13 +1,12 @@
 package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
+import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 import adt.hashtable.hashfunction.HashFunctionQuadraticProbing;
 
-public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
-		extends AbstractHashtableOpenAddress<T> {
+public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable> extends AbstractHashtableOpenAddress<T> {
 
-	public HashtableOpenAddressQuadraticProbingImpl(int size,
-			HashFunctionClosedAddressMethod method, int c1, int c2) {
+	public HashtableOpenAddressQuadraticProbingImpl(int size, HashFunctionClosedAddressMethod method, int c1, int c2) {
 		super(size);
 		hashFunction = new HashFunctionQuadraticProbing<T>(size, method, c1, c2);
 		this.initiateInternalTable(size);
@@ -15,13 +14,25 @@ public class HashtableOpenAddressQuadraticProbingImpl<T extends Storable>
 
 	@Override
 	public void insert(T element) {
-		
+		int probe = 0;
+		int index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, probe);
+		while (table[index] != null && probe < table.length) {
+			index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, ++probe);
+			COLLISIONS++;
+		}
+		if (probe < table.length) {
+			table[index] = element;
+			elements++;
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int probe = 0;
+		int index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, probe);
+		while(table[index] != null && !table[index].equals(element) && probe < table.length) {
+			index = ((HashFunctionOpenAddress<T>) hashFunction).hash(element, ++probe);
+		}
 	}
 
 	@Override
